941dcc07d946730ee0fcf22529e4097b5fb0e9dd
Rodolfo Barbosa <rodolfo@dtsqd.com>
2026-06-22 08:22:18 -0300

    fix: update imports for langchain 0.3 compat, add langchain-text-splitters dep

--- src/loader.py
+++ src/loader.py
@@ -1,6 +1,6 @@
 
 from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
